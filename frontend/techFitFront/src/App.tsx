d9e0473e2baa5f30e30d19bb6c07a6ca8dbe3570
KronembergerYgor <ygorkronemberger@gmail.com>
2026-04-13 22:24:50 -0300

    .
    Signed-off-by: KronembergerYgor <ygorkronemberger@gmail.com>

--- frontend/techFitFront/src/App.tsx
+++ frontend/techFitFront/src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Eu amo meu nénem</h1>
+          <h1>Eu amo meu nénem - tesrt</h1>
           <p>
             Edit <code></code>
           </p>
