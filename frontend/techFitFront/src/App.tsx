918620c6d7b9ca926b37aff0602cd4dc27cbfdef
KronembergerYgor <ygorkronemberger@gmail.com>
2026-04-24 23:07:19 -0300

    Testando responsividade
    Signed-off-by: KronembergerYgor <ygorkronemberger@gmail.com>

--- frontend/techFitFront/src/App.tsx
+++ frontend/techFitFront/src/App.tsx
@@ -1,13 +1,30 @@
-import React from 'react'
+import { useEffect } from 'react'
 import Header from './components/header/Header';
 import FirstSession from './components/body/FirstSession';
 import SecondSession from './components/body/SecondSession';
 import ThirdSession from './components/body/ThirdSession';
 import './styles/App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import logo from './assets/LogoNavigate.png';
+
 
 
 
 function App() {
+
+ useEffect(() => {
+   const link =
+      document.querySelector("link[rel~='icon']") ||
+      document.createElement("link");
+
+    link.setAttribute("rel", "icon");
+    link.setAttribute("type", "image/png");
+    link.setAttribute("href", logo);
+
+    document.head.appendChild(link);
+
+    document.title = "TechFit Front";
+  }, []);
 
   return (
     <>
--- frontend/techFitFront/src/components/header/Header.tsx
+++ frontend/techFitFront/src/components/header/Header.tsx
@@ -1,28 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Menu from './titulo/Menu';
 import '../../styles/Header.css';
 import '../Button';
 import Button from '../Button';
+import logo from '../../assets/LogoCompleta.png';
 
 function Header() {
+    const [open, setOpen] = useState(false);
   return (
   <header className="header-container">
 
-      <ul className='nav-container'>
-          
-          <Menu name="ALUNOS" url="#"/>
-          <Menu name="PROFISSIONAIS" url="#"/>
-      
+      {/* botão hambúrguer */}
+      <button
+        className="hamburger"
+        onClick={() => setOpen(!open)}
+      >
+        ☰
+      </button>
+
+      <ul className={`nav-container ${open ? 'active' : ''}`}>
+        <Menu name="ALUNOS" url="#" />
+        <Menu name="PROFISSIONAIS" url="#" />
+
+        <img src={logo} alt="Logo" width="19%"/>
+
+        <Menu name="REGISTRO" url="#" />
+        <li className="listItem">
+          <Button classNameText="buttonRegister" title="Cadastre-se" />
+        </li>
       </ul>
 
-
-      <ul className='nav-container'>
-        
-        <Menu name="REGISTRO" url="#"/>
-        <li className="listItem"> <Button classNameText="buttonRegister" title="Cadastre-se"/></li>
-
-      </ul>
-  </header>
+    </header>
   );
 }
 
