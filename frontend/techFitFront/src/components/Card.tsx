31c42da64f228ab7bdd3bf165eb2c01f79dcc208
KronembergerYgor <ygorkronemberger@gmail.com>
2026-05-26 22:33:19 -0300

    Trabalhando nos cards e titulos
    Signed-off-by: KronembergerYgor <ygorkronemberger@gmail.com>

--- frontend/techFitFront/src/components/Card.tsx
+++ frontend/techFitFront/src/components/Card.tsx
@@ -1,17 +1,127 @@
-import React from 'react';
+import { type ReactNode, useState } from 'react';
+import { FiChevronDown, FiChevronUp } from 'react-icons/fi';
 
 interface props {
   titleCard: string;
   textCard: string;
+  icon?: ReactNode;
 }
 
-function Card({titleCard, textCard}: props) {
-  return (
-    <div className="card">
-      <h4>{titleCard}</h4>
-      <p>{textCard}</p>
-    </div>
-  );
+
+function Card({titleCard, textCard, icon}: props) {
+
+  const styles = {
+    iconStyle:{
+      fontSize:"5rem", 
+      padding:"0", 
+      margin: "0", 
+      color: "#db8300",
+      filter: 'drop-shadow(4px 2px 2px rgba(0, 0, 0, 0.5))'
+    },
+    titleStyle:{
+      padding:"0", 
+      margin: "0"
+    },
+    styleButton :{
+      background: 'none',
+      color: '#e18600', // O laranja do seu site TechFit
+      cursor: 'pointer',
+      display: 'inline-flex',
+      alignItems: 'center',
+      gap: '4px',
+      fontWeight: '600',
+      fontSize: '0.85rem',
+      marginTop: "0.5rem",
+      marginBottom: "0.5rem",
+      padding: '0px',
+      transition: 'transform 1s',
+      border: 'solid 0.5px #4b4b4b',
+      borderRadius: '9px',
+      boxShadow: '0px 1px 2px #000'
+    }
+  } 
+
+    const descriptrionStyle: React.CSSProperties ={
+      overflowWrap: 'break-word', 
+      wordBreak: 'break-word',
+      color: '#000000', 
+      margin: '0.5rem 0.5rem 1.5rem', 
+      lineHeight: '1.6'
+    }
+
+  const [estaExpandido, setEstaExpandido] = useState(false);
+
+  const limite = 170;
+  const sizeIconRow = 20;
+  let textoExibido: string;
+  let validateSizeText: boolean; 
+
+  console.log(textCard.length)
+
+  if (textCard.length <= limite) {
+    textoExibido = textCard;
+    validateSizeText = true;
+  }else{
+    // Criamos as duas versões do texto
+    const textoCortado = `${textCard.slice(0, limite)}...`;
+    const textoCompleto = textCard;
+    textoExibido = estaExpandido ? textoCompleto : textoCortado;
+    validateSizeText = false;
+
+  }
+
+
+  if(validateSizeText){
+
+     return (
+        <div className="card">
+          <h2 style={styles.iconStyle}>{icon}</h2>
+          <h3 style={styles.titleStyle}>{titleCard}</h3>
+          <div className={'wrapper-animado'}>
+            <hr />
+            <p className="card-texto" style={descriptrionStyle}>
+              {textoExibido}
+            </p>
+          </div>
+
+        </div>
+      );
+
+  }else{
+
+    return (
+      <div className="card">
+        <h2 style={styles.iconStyle}>{icon}</h2>
+        <h3 style={styles.titleStyle}>{titleCard}</h3>
+        <div className={`wrapper-animado ${estaExpandido ? 'expandido' : 'recolhido'}`}>
+          <hr />
+          <p className="card-texto" style={descriptrionStyle}>
+            {textoExibido}
+          </p>
+        </div>
+
+        <button 
+          style={styles.styleButton} 
+          onClick={() => setEstaExpandido(!estaExpandido)}
+        >
+          {estaExpandido ? (
+            <>
+              <FiChevronUp size={sizeIconRow} />
+            </>
+          ) : (
+            <>
+              <FiChevronDown size={sizeIconRow} />
+            </>
+          )}
+        </button>
+      </div>
+    );
+
+  }
+
+  
+
+  
 }
 
 export default Card;
--- frontend/techFitFront/src/components/body/SecondSession.tsx
+++ frontend/techFitFront/src/components/body/SecondSession.tsx
@@ -1,21 +1,60 @@
 import React from 'react';
 import '../../styles/body/SecondSession.css';
 import TitleSmall from './TitleSmall';
+import { FaWallet } from "react-icons/fa";
+import { IoBarChart } from "react-icons/io5";
+import { LuBrainCircuit } from "react-icons/lu";
 import Card from '../Card';
 
+const min = 1;
+const max = 1000;
+
+function randomNumber() {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+
+
+
 function SecondSession() {
+
+    const sizeIcons = 65;
+
+    const infosButtons = [
+    {
+      id: randomNumber(),
+      icone: <FaWallet size={sizeIcons} />,
+      titulo: "Gestão e evolução",
+      descricao: "Conecte-se com os melhores profissionais da sua região avaliados por nossa comunidade."
+    },
+    {
+      id: randomNumber(),
+      icone: <IoBarChart size={sizeIcons} />,
+      titulo: "Tudo em um só lugar",
+      descricao: "Acompanhe seus treinos, dietas e evolução de cargas através de gráficos de performance.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    },
+    {
+      id: randomNumber(),
+      icone: <LuBrainCircuit size={sizeIcons} />,
+      titulo: "Planejamento e inteligência",
+      descricao: "Centralize seus agendamentos, treinos passados e histórico de pagamentos sem complicações.aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa"
+    },
+  ];
+
+
   return <>
 
-    <div className="containerSecond containers" style={{ border: "1px solid #000", marginTop: '1rem'}}>
-      <TitleSmall titleText='ALUNOS: ENCONTRE E EVOLUA'/>
+    <div className="containerSecond containers">
+      <h3 className='titleSecondSession'> <span> <span style={{color: "#db8300"}} >ALUNOS:</span> ENCONTRE E EVOLUA</span> </h3>
 
       <div className='containerCards'>
-        <Card titleCard="Titulo Teste" textCard="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." />
-        <Card titleCard="Titulo Teste" textCard="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." />
-        <Card titleCard="Titulo Teste" textCard="Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum." />
 
+        {infosButtons.map((info) => (
+          <Card key={info.id} icon={info.icone} textCard={info.descricao} titleCard={info.titulo} />
+        ))}
+      
 
-
+      
       </div>
 
     </div>
